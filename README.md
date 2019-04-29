846164fa8c645341c68de36562850534e5aa0376
Dana <lockwood.da88@gmail.com>
2019-04-29 14:49:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# About
+
+A simple terminal program that uses the OpenWeatherMap API,
+and the requests library to collect and display local weather data.
+
+## Tools Used
+
+* Python3 
+* Requests
+* Pipenv
+
+## Ongoing and Future Plans
+
+* Display 5-day forecast.
---
+++ weather.py
@@ -0,0 +1,40 @@
+import requests
+
+
+def get_current_weather():
+
+    # Define the GET request to the API
+    # Get current weather data from Seattle
+    current_weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=Seattle,us&APPID=ffc5321958cd581c26b5965c4947ac9f&units=imperial'
+    response = requests.get(current_weather_url)
+    data = response.json()
+
+    temp = data['main']['temp']
+    ws = data['wind']['speed']
+    description = data['weather'][0]['description']
+
+    print()
+    print("The current weather for Seattle is: ")
+    print() # Blank line for readability
+
+    print('Temperature: {}'.format(temp) + " degrees")
+    print('Wind Speed: {}'.format(ws) + " mph")
+    print('Summary: {}'.format(description))
+    print()
+
+# Get the 5-day forecast for Seattle
+def five_day():
+
+    five_day_url = 'http://api.openweathermap.org/data/2.5/forecast?q=Seattle,us&APPID=ffc5321958cd581c26b5965c4947ac9f&units=imperial'
+    response = requests.get(five_day_url)
+    data = response.json()
+
+    print(data)
+
+
+def main():
+
+    get_current_weather()
+
+
+main()
