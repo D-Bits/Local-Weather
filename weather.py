002095e83312e802d5df097d23526567bd3a17a8
Dana <lockwood.da88@gmail.com>
2019-05-28 12:58:22 -0700

    Added current datetime

--- weather.py
+++ weather.py
@@ -1,12 +1,25 @@
-import requests, os
+import requests, os, time, datetime
+from datetime import datetime
 
+
+# Get the current day and time
+def get_current_datetime():
+
+    now = datetime.now()
+    # Print the current date and time
+    print()
+    print("Current Date & Time: ")
+    print(now.month, "-", now.day, "-", now.year)
+    print(now.hour, ":", now.minute)
+    print()
+    print()
 
 def current_weather():
 
     # Define the GET request to the API
     # Get current weather data from Seattle
 
-    # Old attempts
+    # Old, parameterized attempt
     """
     # api_key = os.getenv('WEATHER_API_KEY')
     # api_url = requests.get(url="https://api.openweathermap.org/data/2.5/weather", params={'q': 'Seattle,us', 'APPID': api_key, 'units': 'imperial'})
@@ -20,20 +33,17 @@ def current_weather():
     humidity = data['main']['humidity']
     description = data['weather'][0]['description']
 
-    print()
+
     print("The current weather for Seattle is: ")
     print() # Blank line for readability
 
-    print("The current temp is: ") 
-    print(temp)
+    print("The current temp is:", temp, "degrees") 
     print()
 
-    print("Wind Speed: ")
-    print(ws)
+    print("Wind Speed:", ws, "mph")
     print()
 
-    print("Humidity: ")
-    print(humidity)
+    print("Humidity:", humidity)
     print()
 
     print('Summary: ' + description)
@@ -51,10 +61,9 @@ def five_day():
     print(data)
 
 
-def main():
 
+if __name__ == "__main__":
+
+    get_current_datetime()
     current_weather()
     # five_day()
-
-
-main()
