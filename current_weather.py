c9b9c19936df018283055de28fc5dcc0dd64be36
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-06-05 01:02:10 -0700

    Added main.py

--- current_weather.py
+++ current_weather.py
@@ -79,20 +79,4 @@ def metric_current_weather():
 
     print('Summary:', description)
     print()
-
-
-if __name__ == "__main__":
-
-    get_current_datetime()
-    units = int(input('Please enter 1 for metric weather, or 2 for imperial units: '))
-    
-    print() # Blank line for readability
-
-    # If user enters 1, show weather data in metric. If 2, use imperial units
-    if units == 1:
-        metric_current_weather()
-    elif units == 2:
-        imperial_current_weather()
-    else:
-        raise Exception("Please enter 1 or 2.")
     
---
+++ main.py
@@ -0,0 +1,19 @@
+from current_weather import get_current_datetime, imperial_current_weather, metric_current_weather
+
+
+if __name__ == "__main__":
+
+    get_current_datetime()
+    units = int(input('Please enter 1 for metric units, or 2 for imperial units: '))
+    
+    print() # Blank line for readability
+
+    # If user enters 1, show weather data in metric. If 2, use imperial units
+    if units == 1:
+        metric_current_weather()
+    elif units == 2:
+        imperial_current_weather()
+    else:
+        raise Exception("Please enter only 1 or 2.")
+    
+    
